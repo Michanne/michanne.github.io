ac2eb278ce00d11203b45f5f7f599e25f91b8caa
michael <lavontaebrown@gmail.com>
2016-01-03 16:49:00 -0500

    Test

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 (function(){
 	var app = angular.module('main', [ ]);
 
+	var myName = "michanne";
+
 	app.controller('mainController', function(){
 		this.githubName = myName;
 	});
-
-	var myName = "michanne";
 })();
