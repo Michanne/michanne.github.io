a2fd594b6c6ee5c7792eb380a6493bb934d9850f
michael <lavontaebrown@gmail.com>
2016-01-03 17:05:00 -0500

    Tesy

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-var app = angular.module('mainApp', []);
+var mainApp = angular.module('mainApp', []);
 
 (function(){
-	app.controller('mainController', function(){
+	mainApp.controller('mainController', function(){
 		this.githubName = 'michanne';
 	});
 })();
